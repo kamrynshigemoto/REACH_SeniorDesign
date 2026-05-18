f76f3c81d6d2fa44a6f02e9b2c57c84260c7b682
J-PachEE <jairpacheco8787@gmail.com>
2026-05-18 03:22:12 -0700

    Update LCDManager.h

--- ESP8266/Final Code/Header Files/LCDManager.h
+++ ESP8266/Final Code/Header Files/LCDManager.h
@@ -1,3 +1,17 @@
+//-----------------------------
+// Title: LCDManager.h
+//-----------------------------
+// Purpose: Declares the LCDManager class used to control a 20x4 I2C LCD display.
+// The display shows USB voltage, current, ON/OFF state, supply voltage, and schedule ACK.
+// Dependencies: Arduino.h, LiquidCrystal_I2C.h
+// Compiler: Arduino IDE / PlatformIO for ESP8266
+// Author: Juan Jimenez
+// OUTPUTS: LCD display update interface
+// INPUTS: USB voltages, USB currents, supply voltage, USB states, schedule data,
+//         epoch time, schedule ACK flag
+// Versions:
+//      V1.0: 5/18/2026 - Declared LCD display manager for USB telemetry dashboard
+//-----------------------------
 #pragma once
 
 #include <Arduino.h>
